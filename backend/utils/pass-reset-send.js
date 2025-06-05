cce512cfca2cc3122dbba68ff5090bb3f535899f
Tusher <tousherfana10@gmail.com>
2025-06-05 18:28:11 +0800

    update backend link

--- backend/utils/pass-reset-send.js
+++ backend/utils/pass-reset-send.js
@@ -1,10 +1,10 @@
 const {Resend} = require('resend');
 const resetPasswordTemplate =require('./pass-reset-temp');
 const resend = new Resend(process.env.EMAIL_VERIFICATION_API);
-
+const APIURI=process.env.BACKEND_URI_KEY
 const forgetEmail = async(email, name, tokenReset)=>{
     
-    const resetLink =`http://localhost:5000/api/auth/reset-password/${tokenReset}`;
+    const resetLink =`${APIURI}/api/auth/reset-password/${tokenReset}`;
     try{
         const {data,erro}= await resend.emails.send({
             from: 'onboarding@resend.dev',
--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,8 +2,9 @@
 const signupConfirmTemp = require('./signupConfimTemp');
 const {Resend}=require('resend')
 const resend = new Resend(process.env.EMAIL_VERIFICATION_API)
+const APIURI=process.env.BACKEND_URI_KEY
  const sendEmail = async (email, name,jwtToken)=>{
-const verificationLink= `http://localhost:5000/api/auth/verification/${jwtToken}`
+const verificationLink= `${APIURI}/api/auth/verification/${jwtToken}`
     try{
         const {data, error }= await resend.emails.send({
             from:'onboarding@resend.dev',
